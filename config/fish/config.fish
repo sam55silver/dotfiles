b906cfc4f5bd7c69b995b731991fb4b00d83354f
Sam Silver <sam55silver@gmail.com>
2024-06-23 21:47:35 -0300

    update fish vim mode and tmux colour to new colour scheme

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,19 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
+
+    # I'm trying to grow a neckbeard
+    # fish_vi_key_bindings
+    # Set the cursor shapes for the different vi modes.
+    set fish_cursor_default     block      blink
+    set fish_cursor_insert      line       blink
+    set fish_cursor_replace_one underscore blink
+    set fish_cursor_visual      block
+
+    function fish_user_key_bindings
+      # Execute this once per mode that emacs bindings should be used in
+      fish_default_key_bindings -M insert
+      fish_vi_key_bindings --no-erase insert
+    end
 end
 
-set PATH $PATH /usr/local/nvim/bin $HOME/.cargo/bin
+set PATH $PATH $HOME/.cargo/bin
 source ~/.asdf/asdf.fish
